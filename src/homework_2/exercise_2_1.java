f863255c72c232481acbe566587a1d6e794588de
yar-klepikov <i.klepikov@hh.ru>
2022-08-25 18:47:00 +0300

    вторая дз

---
+++ src/homework_2/exercise_2_1.java
@@ -0,0 +1,27 @@
+package homework_2;
+
+import java.util.Scanner;
+
+public class exercise_2_1 {
+
+    public static void main(String[] args) {
+        Scanner scann = new Scanner(System.in);
+        int centr_x = scann.nextInt();
+        int centr_y = scann.nextInt();
+        int radius = scann.nextInt();
+        int x = scann.nextInt();
+        int y = scann.nextInt();
+
+        if (Raz(x, y, centr_x, centr_y, radius)) {
+            System.out.println("in a circle");
+        } else {
+            System.out.println("outside the circle");
+
+
+        }
+    }
+
+    static boolean Raz(double x, double y, double centr_x, double centr_y, double r) {
+        return (Math.sqrt(x - centr_x)) + (Math.sqrt(y - centr_y)) <= Math.sqrt(r);
+    }
+}
